b37b225e3cb48e03890881afafcc323247fba127
Frederyck9902 <frederyck9902@gmail.com>
2021-10-18 21:08:18 -0500

    Proyecto Completo

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,9 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'proyecto005';
+  mensaje = '';
+
+  actualizar(t: string){
+    this.mensaje = t + '(se actualiza cada 10 segundos)';
+  }
 }
